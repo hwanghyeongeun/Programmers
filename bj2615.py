3eea4582bb6530537a162929652094056896dcf1
hwanghyeongeun <hwanghyeongeun@gmail.com>
2022-01-08 12:13:12 +0900

    20220101 1213 동쪽완성

--- bj2615.py
+++ bj2615.py
@@ -2,15 +2,52 @@
 
 #0,0부터 시작점을 잡고
 #5개씩 더해서 5가 나오면 흑 승 / 10이 나오면 백 승?
-#방향은 가로 세로 남서쪽 남동쪽 4개 함수로 빼고
+#방향은 동쪽 남쪽 남서쪽 남동쪽 4개 함수로 빼고
 #메인에서 함수 호출해서 돌려보고 시작점 전달
+
+global gx, gy
+
+#
+def east(i, j):
+    for i in range(19):
+        for j in range(15):
+            sumb = 0 #초기화
+            sumw = 0 #초기화
+            k = 0
+            while k < 5:
+                if board[i][j+k] == 1:
+                    sumb += board[i][j+k]
+                if board[i][j+k] == 2:
+                    sumw += board[i][j+k]
+                if sumb == 5:
+                    gx = i
+                    gy = j
+                    print ("winner is black (way easy)", gx, gy)
+                    break
+                if sumw == 10:
+                    gx = i
+                    gy = j
+                    print ("winner is white (way easy)", gx, gy)
+                    break
+                k += 1
 
 
 #바둑판
 board = []
-board.append([0]*19)
 for _ in range(19):
-    ls = [0] + list(map(int,input().split())) +[0]
+    ls = list(map(int,input().split()))
     board.append(ls)
-board.append([0]*19)
 
+print("############################################################")
+
+#바둑판 출력
+for i in range(19):
+    for j in range(19):
+        print(board[i][j], end=' ')
+    print()
+
+
+print("############################################################")
+
+#함수 호출 부분
+east(0,0)
