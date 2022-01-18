d60c987ebf0de98ca37e4a525f417461840dc3a0
hwanghyeongeun <hwanghyeongeun@gmail.com>
2022-01-18 22:41:46 +0900

    선주선배님 ♡

--- baekjoon/16953 A→B.py
+++ baekjoon/16953 A→B.py
@@ -17,10 +17,9 @@ a, b = map(int, input().split())
 count = 1
 
 while a<b:
-    if b%2 == 0:
-        b /= 2
-    elif b%10 == 1:
-        b //= 10
+    if b%2 == 0 : b /= 2
+    elif b%10 == 1: b //= 10
+    elif b%10 > 1: break
     count += 1
     
 if a==b:
